7c4b5fc0030c0f191401ce105f5e569dac036ac6
RealFatCat <trashmessage@yandex.ru>
2026-01-13 23:54:36 +0300

    Support custom characters; Walking Cthulhu as example; Add public methods to work with LEDs, print raw data, etc; some refactoring; update docs

--- main.go
+++ main.go
@@ -7,6 +7,18 @@ import (
 	lcd1602 "github.com/realfatcat/lcd1602/pkg/lcd"
 )
 
+// custom Cthulhu character
+var cthulhu = [8]byte{
+	0b01110,
+	0b11111,
+	0b10101,
+	0b11111,
+	0b01110,
+	0b11111,
+	0b10101,
+	0b10101,
+}
+
 func main() {
 	isBacklightOn := true
 	cols := 16
@@ -23,10 +35,74 @@ func main() {
 	if err := lcd.Print("=<^ _ ^>=", 1, 7); err != nil {
 		log.Fatal(err)
 	}
+
+	wk := newKtulhu(
+		1, // start row
+		0, // start column
+		5, // turn back column
+		0, // turn forward column
+		1, // at first move to the right
+		0, // char location: CGRAM location, where to save custom Cthulhu character (0-7)
+		lcd,
+	)
 	for {
 		if err := lcd.Print(time.Now().Format(time.TimeOnly), 0, 0); err != nil {
 			log.Fatal(err)
 		}
 		time.Sleep(1 * time.Second)
+		wk.MakeStep()
 	}
+}
+
+type walkingCthulhu struct {
+	direction      int
+	turnBackCol    int
+	turnForwardCol int
+	curCol         int
+	curRow         int
+	lcd            *lcd1602.LCD1602
+	kthulhu        [8]byte
+	charLocation   byte
+}
+
+func newKtulhu(startRow, startCol, turnBackCol, turnForwardCol, direction int, charLocation byte, lcd *lcd1602.LCD1602) *walkingCthulhu {
+	direct := 1
+	if direction < 0 {
+		direct = -1
+	}
+
+	if err := lcd.UploadCustomChar(charLocation, cthulhu); err != nil {
+		log.Fatal(err)
+	}
+
+	if err := lcd.PrintRAW(charLocation, startRow, startCol); err != nil {
+		log.Fatal(err)
+	}
+
+	return &walkingCthulhu{
+		kthulhu:        cthulhu,
+		lcd:            lcd,
+		curRow:         startRow,
+		curCol:         startCol,
+		turnBackCol:    turnBackCol,
+		turnForwardCol: turnForwardCol,
+		direction:      direct,
+		charLocation:   charLocation,
+	}
+}
+
+func (wk *walkingCthulhu) MakeStep() {
+	switch wk.direction {
+	case 1:
+		if wk.curCol == wk.turnBackCol {
+			wk.direction = -1
+		}
+	case -1:
+		if wk.curCol == wk.turnForwardCol {
+			wk.direction = 1
+		}
+	}
+	wk.lcd.Print(" ", wk.curRow, wk.curCol)
+	wk.curCol += wk.direction
+	wk.lcd.PrintRAW(wk.charLocation, wk.curRow, wk.curCol)
 }
